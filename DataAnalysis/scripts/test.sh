44fb22aaa761b51467e97e5debd48db94720c23f
Tobias Pett <t.pett@tu-braunschweig.de>
2019-10-23 18:28:27 +0200

    update test.sh

--- DataAnalysis/scripts/test.sh
+++ DataAnalysis/scripts/test.sh
@@ -4,14 +4,28 @@
 
 
 PATH_Automotive02=auto
-echo ${PATH_Automotive02}
+echo ${PATH_Automotive02} >> test.txt
 
 PATH_Automotive02=automot
-echo ${PATH_Automotive02}
-
+echo ${PATH_Automotive02} >> test.txt
 
 PATH_Automotive02=automotive02
-echo ${PATH_Automotive02}
+echo ${PATH_Automotive02} >> test.txt
+
+for i in 1 2 3 4 5
+do
+start=`date +%s`
+sleep 20s
+end=`date +%s`
+runtime=$((end-start))
+echo "runtime;"${runtime}";run finished" >> test.txt
+done
+
+
+
+
+
+
 
 
 
