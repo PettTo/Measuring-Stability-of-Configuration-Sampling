09a52de6a2a2656bb1a3fc538a5941361bbea933
Tobias Pett <t.pett@tu-braunschweig.de>
2019-10-24 09:56:38 +0200

    Update: correct mistakes in compression script

--- DataAnalysis/scripts/utility/compressModels.sh
+++ DataAnalysis/scripts/utility/compressModels.sh
@@ -5,22 +5,39 @@ cd ..
 PATH_CURRENT=$PWD
 PATH_DATA=${PATH_CURRENT}/data
 
+PATH_BussyBox=${PATH_DATA}/busybox/models
 PATH_Automotive02=${PATH_DATA}/automotive02/models
 PATH_FinancialServices01=${PATH_DATA}/financialServices01/models
-PATH_BussyBox=${PATH_DATA}/busybox_monthlySnapshot/models
+PATH_BussyBox_monthly=${PATH_DATA}/busybox_monthlySnapshot/models
 
 ####### busyBox
-#cd ${PATH_BussyBox}
-#for year in *; do
-#	tar -zcvf ${year}.tar.gz ${year}
-#done
+cd ${PATH_BussyBox}
+for year in ${PATH_BussyBox}/*; do
+	echo ${year}
+	cd ${year}
+		rm *.tar.gz
+	for run in *; do
+		tar -zcvf ${run}.tar.gz ${run}
+	done
+done
 
-cd ${PATH_FinancialServices01}
+####### busyBox monthly
+cd ${PATH_BussyBox_monthly}
+rm *.tar.gz
 for year in *; do
 	tar -zcvf ${year}.tar.gz ${year}
 done
 
+####### financialServices01
+cd ${PATH_FinancialServices01}
+rm *.tar.gz
+for year in *; do
+	tar -zcvf ${year}.tar.gz ${year}
+done
+
+####### automotive02
 cd ${PATH_Automotive02}
+rm *.tar.gz
 for year in *; do
 	tar -zcvf ${year}.tar.gz ${year}
 done
