04c4a7c3dd4087a2fe2a31c4c2cd69eaff5c65c6
Tobias Pett <t.pett@tu-braunschweig.de>
2019-10-24 16:56:39 +0200

    Update: refactor compression and decrompression script

--- DataAnalysis/scripts/utility/compressSamples.sh
+++ DataAnalysis/scripts/utility/compressSamples.sh
@@ -7,43 +7,66 @@ PATH_DATA=${PATH_CURRENT}/data/
 
 PATH_Automotive02=${PATH_DATA}/automotive02/samples/
 PATH_FinancialServices01=${PATH_DATA}/financialServices01/samples/
-PATH_BussyBox=${PATH_DATA}/busybox_monthlySnapshot/samples/
+PATH_BussyBox_Monthly=${PATH_DATA}/busybox_monthlySnapshot/samples/
+PATH_BussyBox=${PATH_DATA}/busybox/samples/
 
 ####### automotive02
-#for version in ${PATH_Automotive02}/*; do
-#	for procedure in ${version}/*; do 
-#		cd ${procedure}
-#		for run in *; do
-#			tar -zcvf ${run}.tar.gz ${run}
-#		done
-#	done
-#done
-
-####### finacnialServices01
-#for version in ${PATH_FinancialServices01}/*; do
-#	for procedure in ${version}/*; do 
-#		cd ${procedure}
-#		for run in *; do
-#			tar -zcvf ${run}.tar.gz ${run}
-#		done
-#	done
-#done
-
-####### busyBox
-#cd ${PATH_BussyBox}
-#echo ${PATH_BussyBox}
-	for version in ${PATH_BussyBox}/*; do 
-		#echo ${version}
+	for version in ${PATH_Automotive02}/*; do 
 		for procedure in ${version}/*; do
-		#	cd ${procedure}
-			#echo ${procedure}
+			echo ${procedure}
 			cd ${procedure}
+			#echo ${procedure}
+			rm *.tar.gz
 			for run in *; do
 				#echo ${run}
 				tar -zcvf ${run}.tar.gz ${run}
 			done
 		done
 	done
+
+####### finacnialServices01
+	for version in ${PATH_FinancialServices01}/*; do 
+		for procedure in ${version}/*; do
+			echo ${procedure}
+			cd ${procedure}
+			#echo ${procedure}
+			rm *.tar.gz
+			for run in *; do
+				#echo ${run}
+				tar -zcvf ${run}.tar.gz ${run}
+			done
+		done
+	done
+
+####### busyBox monthly
+	for version in ${PATH_BussyBox_Monthly}/*; do 
+		for procedure in ${version}/*; do
+			echo ${procedure}
+			cd ${procedure}
+			#echo ${procedure}
+			rm *.tar.gz
+			for run in *; do
+				#echo ${run}
+				tar -zcvf ${run}.tar.gz ${run}
+			done
+		done
+	done
+	
+####### busyBox currently not needed
+#cd ${PATH_BussyBox}
+#echo ${PATH_BussyBox}
+#	for version in ${PATH_BussyBox}/*; do 
+		#echo ${version}
+#		for procedure in ${version}/*; do
+		#	cd ${procedure}
+			#echo ${procedure}
+#			cd ${procedure}
+#			for run in *; do
+				#echo ${run}
+#				tar -zcvf ${run}.tar.gz ${run}
+#			done
+#		done
+#	done
 
 
 
--- DataAnalysis/scripts/utility/decompressSamples.sh
+++ DataAnalysis/scripts/utility/decompressSamples.sh
@@ -8,18 +8,7 @@ PATH_DATA=${PATH_CURRENT}/data
 PATH_Automotive02=${PATH_DATA}/automotive02/samples
 PATH_FinancialServices01=${PATH_DATA}/financialServices01/samples
 PATH_BussyBox=${PATH_DATA}/busybox/samples
-
-####### unzip finserv gz
-#for version in ${PATH_FinancialServices01}/*; do
-#	for procedure in ${version}/*; do 
-#		#cd ${procedure}
-#		for run in ${procedure}/*; do
-#			for conf in ${run}/*; do
-#				gunzip ${conf}
-#			done
-#		done
-#	done
-#done
+PATH_BussyBox_Monthly=${PATH_DATA}/busybox_monthlySnapshot/samples
 
 ####### automotive02
 for version in ${PATH_Automotive02}/*; do
@@ -32,17 +21,7 @@ for version in ${PATH_Automotive02}/*; do
 done
 
 ####### finacnialServices01
-#for version in ${PATH_FinancialServices01}/*; do
-#	for procedure in ${version}/*; do 
-#		cd ${procedure}
-#		for run in *.tar.gz; do
-#			tar -xzvf ${run}
-#		done
-#	done
-#done
-
-####### busyBox
-for version in ${PATH_BussyBox}/*; do
+for version in ${PATH_FinancialServices01}/*; do
 	for procedure in ${version}/*; do 
 		cd ${procedure}
 		for run in *.tar.gz; do
@@ -50,6 +29,26 @@ for version in ${PATH_BussyBox}/*; do
 		done
 	done
 done
+
+####### busyBox monthly
+for version in ${PATH_BussyBox_Monthly}/*; do
+	for procedure in ${version}/*; do 
+		cd ${procedure}
+		for run in *.tar.gz; do
+			tar -xzvf ${run}
+		done
+	done
+done
+
+####### busyBox currently not used 
+#for version in ${PATH_BussyBox}/*; do
+#	for procedure in ${version}/*; do 
+#		cd ${procedure}
+#		for run in *.tar.gz; do
+#			tar -xzvf ${run}
+#		done
+#	done
+#done
 
 
 
